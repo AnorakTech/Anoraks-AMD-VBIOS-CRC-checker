ac61d8765fa711ccd73090106ae4333bd3ffd08a
Anorak <anorak@anorak.tech>
2016-12-04 20:40:51 +0100

    Add files via upload

---
+++ AnoraksCRC/Form1.Designer.vb
@@ -0,0 +1,156 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.btnOpen = New System.Windows.Forms.Button()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.LinkLabel1 = New System.Windows.Forms.LinkLabel()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.GroupBox1.SuspendLayout()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'btnOpen
+        '
+        Me.btnOpen.Location = New System.Drawing.Point(249, 17)
+        Me.btnOpen.Name = "btnOpen"
+        Me.btnOpen.Size = New System.Drawing.Size(75, 23)
+        Me.btnOpen.TabIndex = 0
+        Me.btnOpen.Text = "Check CRC"
+        Me.btnOpen.UseVisualStyleBackColor = True
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.TextBox2)
+        Me.GroupBox1.Controls.Add(Me.TextBox1)
+        Me.GroupBox1.Controls.Add(Me.Label2)
+        Me.GroupBox1.Controls.Add(Me.Label1)
+        Me.GroupBox1.Location = New System.Drawing.Point(12, 12)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(231, 80)
+        Me.GroupBox1.TabIndex = 1
+        Me.GroupBox1.TabStop = False
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(119, 45)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(100, 20)
+        Me.TextBox2.TabIndex = 4
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(119, 19)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(100, 20)
+        Me.TextBox1.TabIndex = 3
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(6, 48)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(107, 13)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Expected checksum:"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(20, 22)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(93, 13)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Saved checksum:"
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Image = CType(resources.GetObject("PictureBox1.Image"), System.Drawing.Image)
+        Me.PictureBox1.InitialImage = Nothing
+        Me.PictureBox1.Location = New System.Drawing.Point(245, 39)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(87, 62)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
+        Me.PictureBox1.TabIndex = 2
+        Me.PictureBox1.TabStop = False
+        '
+        'LinkLabel1
+        '
+        Me.LinkLabel1.AutoSize = True
+        Me.LinkLabel1.Location = New System.Drawing.Point(18, 95)
+        Me.LinkLabel1.Name = "LinkLabel1"
+        Me.LinkLabel1.Size = New System.Drawing.Size(100, 13)
+        Me.LinkLabel1.TabIndex = 3
+        Me.LinkLabel1.TabStop = True
+        Me.LinkLabel1.Text = "https://anorak.tech"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(18, 117)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(295, 13)
+        Me.Label3.TabIndex = 4
+        Me.Label3.Text = "Donate ZEC:  t1ZKuTQ7kbFTFArXa1uZDU9THpyU72aJn24"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(333, 138)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.LinkLabel1)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Controls.Add(Me.btnOpen)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Anoraks AMD VBIOS CRC checker v1"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents btnOpen As Button
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label1 As Label
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents LinkLabel1 As LinkLabel
+    Friend WithEvents Label3 As Label
+End Class
---
+++ AnoraksCRC/Form1.vb
@@ -0,0 +1,72 @@
+﻿Imports System.IO
+
+Public Class Form1
+
+    Private Sub btnFixCRC_Click(sender As Object, e As EventArgs) Handles btnOpen.Click
+        Dim myStream As IO.FileStream = Nothing
+        Dim openFileDialog1 As New OpenFileDialog()
+        Dim romStorageBuffer As [Byte]()
+
+        openFileDialog1.InitialDirectory = ""
+        openFileDialog1.Filter = "ROM files (*.rom)|*.rom|BIN files (*.bin)|*.bin|All files (*.*)|*.*"
+        openFileDialog1.FilterIndex = 1
+        openFileDialog1.RestoreDirectory = False
+
+        If openFileDialog1.ShowDialog() = System.Windows.Forms.DialogResult.OK Then
+            Try
+                myStream = openFileDialog1.OpenFile()
+
+                If myStream IsNot Nothing Then
+                    Using br As New BinaryReader(myStream)
+                        romStorageBuffer = br.ReadBytes(myStream.Length)
+
+                        Dim oldchecksum As [Byte] = romStorageBuffer(33)
+                        Dim size As Integer = romStorageBuffer(2) * 512
+                        Dim newchecksum As [Byte] = 0
+                        Dim expected As Integer = 0
+
+
+                        TextBox1.ForeColor = Color.Black
+                        TextBox1.Text = "0x" + oldchecksum.ToString("X")
+
+                        For i As Integer = 0 To size - 1
+                            newchecksum = (CInt(newchecksum) + romStorageBuffer(i)) Mod 256
+                        Next
+
+
+                        If oldchecksum = (CInt(newchecksum) + romStorageBuffer(33)) Mod 256 Then
+                            TextBox2.ForeColor = Color.DarkGreen
+                            TextBox2.Text = "0x" + oldchecksum.ToString("X")
+                        Else
+                            expected = (romStorageBuffer(33) - CInt(newchecksum)) Mod 256
+                            TextBox2.ForeColor = Color.Red
+                            TextBox2.Text = "0x" + expected.ToString("X").Substring(Math.Max(0, expected.ToString("X").Length - 2))
+                        End If
+
+                    End Using
+                End If
+
+
+
+            Catch Ex As Exception
+                MessageBox.Show("Cannot read file from disk. Original error: " & Ex.Message)
+            Finally
+                'Check this again, since we need to make sure we didn't throw an exception on open.
+                If (myStream IsNot Nothing) Then
+                    myStream.Close()
+                End If
+            End Try
+        End If
+    End Sub
+
+    Private Sub LinkLabel1_LinkClicked(sender As Object, e As LinkLabelLinkClickedEventArgs) Handles LinkLabel1.LinkClicked
+        Dim url As String = "https://anorak.tech"
+        Process.Start(url)
+    End Sub
+
+    Private Sub Label3_Click(sender As Object, e As EventArgs) Handles Label3.Click
+        Dim Donation As String = "t1ZKuTQ7kbFTFArXa1uZDU9THpyU72aJn24"
+        My.Computer.Clipboard.SetText(Donation)
+        MessageBox.Show("ZEC address copied to clipboard: " & Donation)
+    End Sub
+End Class
